40b20dfca7dfbe0db4985f11d81c3510f7563d39
ioiiiioi <jefri@mail.to>
2025-09-08 11:20:17 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+// use serde::{Deserialize, Serialize};
+
+
+
+async fn root() -> &'static str {
+    "hello world"
+}
+
+
+#[tokio::main]
+async fn main() {
+    // tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+    .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap()
+
+}
